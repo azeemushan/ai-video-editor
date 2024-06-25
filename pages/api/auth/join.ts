cd8a4f33bf97e4022c8e3be19a017a5c4f061d96
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2024-06-25 13:28:24 +0500

    modify join.ts according to editur requirments part 1

--- pages/api/auth/join.ts
+++ pages/api/auth/join.ts
@@ -1,17 +1,17 @@
 import { hashPassword } from '@/lib/auth';
-import { slugify } from '@/lib/server-common';
+// import { slugify } from '@/lib/server-common';
 import { sendVerificationEmail } from '@/lib/email/sendVerificationEmail';
 import { isEmailAllowed } from '@/lib/email/utils';
 import env from '@/lib/env';
 import { ApiError } from '@/lib/errors';
-import { createTeam, getTeam, isTeamExists } from 'models/team';
+// import { createTeam, getTeam, isTeamExists } from 'models/team';
 import { createUser, getUser } from 'models/user';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { recordMetric } from '@/lib/metrics';
-import { getInvitation, isInvitationExpired } from 'models/invitation';
+// import { recordMetric } from '@/lib/metrics';
+import { getInvitation} from 'models/invitation';
 // import { validateRecaptcha } from '@/lib/recaptcha';
-import { slackNotify } from '@/lib/slack';
-import { Team } from '@prisma/client';
+// import { slackNotify } from '@/lib/slack';
+// import { Team } from '@prisma/client';
 import { createVerificationToken } from 'models/verificationToken';
 import { userJoinSchema, validateWithSchema } from '@/lib/zod';
 
@@ -45,7 +45,7 @@ export default async function handler(
 
 // Signup the user
 const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { name, password, team, inviteToken } = req.body;
+  const { name, password,email, inviteToken } = req.body;
 
   // await validateRecaptcha(recaptchaToken);
 
@@ -53,18 +53,18 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
     ? await getInvitation({ token: inviteToken })
     : null;
 
-  let email: string = req.body.email;
+  // let email: string = req.body.email;
 
   // When join via invitation
-  if (invitation) {
-    if (await isInvitationExpired(invitation.expires)) {
-      throw new ApiError(400, 'Invitation expired. Please request a new one.');
-    }
+  // if (invitation) {
+  //   if (await isInvitationExpired(invitation.expires)) {
+  //     throw new ApiError(400, 'Invitation expired. Please request a new one.');
+  //   }
 
-    if (invitation.sentViaEmail) {
-      email = invitation.email!;
-    }
-  }
+  //   if (invitation.sentViaEmail) {
+  //     email = invitation.email!;
+  //   }
+  // }
 
   validateWithSchema(userJoinSchema, {
     name,
@@ -84,21 +84,21 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   // Check if team name is available
-  if (!invitation) {
-    if (!team) {
-      throw new ApiError(400, 'A team name is required.');
-    }
+  // if (!invitation) {
+  //   if (!team) {
+  //     throw new ApiError(400, 'A team name is required.');
+  //   }
 
-    const slug = slugify(team);
+  //   const slug = slugify(team);
 
-    validateWithSchema(userJoinSchema, { team, slug });
+  //   validateWithSchema(userJoinSchema, { team, slug });
 
-    const slugCollisions = await isTeamExists(slug);
+  //   const slugCollisions = await isTeamExists(slug);
 
-    if (slugCollisions > 0) {
-      throw new ApiError(400, 'A team with this slug already exists.');
-    }
-  }
+  //   if (slugCollisions > 0) {
+  //     throw new ApiError(400, 'A team with this slug already exists.');
+  //   }
+  // }
 
   const user = await createUser({
     name,
@@ -107,19 +107,19 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
     emailVerified: invitation ? new Date() : null,
   });
 
-  let userTeam: Team | null = null;
+  // let userTeam: Team | null = null;
 
   // Create team if user is not invited
   // So we can create the team with the user as the owner
-  if (!invitation) {
-    userTeam = await createTeam({
-      userId: user.id,
-      name: team,
-      slug: slugify(team),
-    });
-  } else {
-    userTeam = await getTeam({ slug: invitation.team.slug });
-  }
+  // if (!invitation) {
+  //   userTeam = await createTeam({
+  //     userId: user.id,
+  //     name: team,
+  //     slug: slugify(team),
+  //   });
+  // } else {
+  //   userTeam = await getTeam({ slug: invitation.team.slug });
+  // }
 
   // Send account verification email
   if (env.confirmEmail && !user.emailVerified) {
@@ -131,18 +131,18 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
     await sendVerificationEmail({ user, verificationToken });
   }
 
-  recordMetric('user.signup');
+  // recordMetric('user.signup');
 
-  slackNotify()?.alert({
-    text: invitation
-      ? 'New user signed up via invitation'
-      : 'New user signed up',
-    fields: {
-      Name: user.name,
-      Email: user.email,
-      Team: userTeam?.name,
-    },
-  });
+  // slackNotify()?.alert({
+  //   text: invitation
+  //     ? 'New user signed up via invitation'
+  //     : 'New user signed up',
+  //   fields: {
+  //     Name: user.name,
+  //     Email: user.email,
+  //     Team: userTeam?.name,
+  //   },
+  // });
 
   res.status(201).json({
     data: {
